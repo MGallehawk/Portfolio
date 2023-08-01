d804ca014ee6ddda4cf6358e74d76c861e5ac813
Mathew Gallehawk <103008703+MGallehawk@users.noreply.github.com>
2023-08-01 19:44:57 +1000

    created the defend script with header notes for team

---
+++ Coit11241 Cyber Security Technologies/peer programing/defend.ps1
@@ -0,0 +1,45 @@
+<#
+peer-programing script for coit11241
+authers:
+--Chelsea Ajisaka,
+--Francis Renzaho,
+--Mathew Gallehawk
+Lecutrer:
+--Jamie Sheilds
+
+Scope:
+-pretest testWDAC() attempts to run unautherised app - reports
+-setupWDAC() - converts policy to .cip
+-enableWDAC-deploys policy
+-re run testwdac() reporting
+-resetWdac() removes block policy
+#>
+
+
+function testWDAC {
+    param (
+        OptionalParameters
+    )
+    
+}
+
+function setupWDAC {
+    param (
+        OptionalParameters
+    )
+    
+}
+
+function enableWDAC() {
+    param (
+        OptionalParameters
+    )
+    
+}
+
+function resetWDAC() {
+    param (
+        OptionalParameters
+    )
+    
+}
