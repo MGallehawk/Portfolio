af6d382290dbd3ebb73ebe6dfa9180c5d0575a43
ChelseaAJ <128450125+ChelseaAJ@users.noreply.github.com>
2023-08-03 19:54:53 +1000

    Update resetWDAC.ps1

--- Coit11241 Cyber Security Technologies/peer programing/resetWDAC.ps1
+++ Coit11241 Cyber Security Technologies/peer programing/resetWDAC.ps1
@@ -1,6 +1,18 @@
 <# 
 This function's purpose is to remove the WDAC block policy.
 Use citool.exe
+
+Look at microsoft
+Use google and chatgpt
+
+setup wdac creates the policy and creates a binary file
+use (get childitem | where extension = ".cip").Name.Replace(".cip", "")     This gives me all of the objects
+Use citool.exe
+
+remove policy PolicyGUID
+
+
+
 #>
 
 function resetWDAC() {
