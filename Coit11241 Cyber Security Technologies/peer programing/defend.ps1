23aaf167919d1daf90d0abd5445e5ed9ab43b96b
ChelseaAJ <128450125+ChelseaAJ@users.noreply.github.com>
2023-08-04 19:01:38 +1000

    Update defend.ps1

--- Coit11241 Cyber Security Technologies/peer programing/defend.ps1
+++ Coit11241 Cyber Security Technologies/peer programing/defend.ps1
@@ -321,8 +321,27 @@ function enableWDAC() {
 
 
 
-function resetWDAC() {
-  
+function resetWDAC()
+{
+
+    $folderPath = "$PSScriptRoot\ActivePolicies"
+
+    if ((Get-ChildItem -Path $folderPath).Count -gt 0)
+    {
+        $policyArray = @((Get-ChildItem -Path $folderPath | where Extension = ".cip").Name.Replace(".cip", ""))
+    
+        foreach ($policyGuid in policyArray)
+        {
+            .\citool.exe --remove-policy $policyGuid
+        }
+
+        gpupdate /force
+
+    }
+    else 
+    {
+        Write-Output "There are no policies currently set by WDAC"
+    }
     
 }
 
