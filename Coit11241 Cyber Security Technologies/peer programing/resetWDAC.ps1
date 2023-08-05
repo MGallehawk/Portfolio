a526218c495be0e3e368d3eb9a7a94c1752e4076
ChelseaAJ <128450125+ChelseaAJ@users.noreply.github.com>
2023-08-05 20:56:27 +1000

    Update resetWDAC.ps1

--- Coit11241 Cyber Security Technologies/peer programing/resetWDAC.ps1
+++ Coit11241 Cyber Security Technologies/peer programing/resetWDAC.ps1
@@ -1,32 +1,27 @@
 <# 
 This function's purpose is to remove the WDAC block policy.
-Use citool.exe
-
-Look at microsoft
-Use google and chatgpt
-
 #>
 
-function resetWDAC()
-{
+function resetWDAC() {
 
-    $folderPath = "$PSScriptRoot\ActivePolicies"
+    $folderPath = $PSScriptRoot + "\ActivePolicies"
 
-    if ((Get-ChildItem -Path $folderPath).Count -gt 0)
-    {
-        $policyArray = @((Get-ChildItem -Path $folderPath | where Extension = ".cip").Name.Replace(".cip", ""))
+    if ((Get-ChildItem -Path $folderPath).Count -gt 0) {
+        $policyArray = @(((Get-ChildItem -Path $folderPath) | Where-Object -Property Extension -like '*.cip').BaseName)# (".cip".Name.Replace(".cip", ""))
     
-        foreach ($policyGuid in policyArray)
-        {
-            .\citool.exe --remove-policy $policyGuid
+        foreach ($policyGuid in $policyArray) {
+            citool.exe --remove-policy $policyGuid
+            
         }
-
-        gpupdate /force
-
     }
-    else 
-    {
+
+    else {
         Write-Output "There are no policies currently set by WDAC"
     }
     
+    #cleanup
+    $a = (Get-ChildItem -Path $folderPath) | Where-Object -Property Extension -like '*.cip'
+    $a | foreach-object {
+        remove-Item -path $_.FullName
+    }
 }
