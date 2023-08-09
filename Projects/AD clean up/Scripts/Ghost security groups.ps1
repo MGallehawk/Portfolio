f90e5581aed8bd5d7b0d03366c9132cfa4302d44
MGallehawk <mathewgallehawk@outlook.com>
2023-08-09 11:29:52 +1000

    added Active directory cleanup script

---
+++ Projects/AD clean up/Scripts/Ghost security groups.ps1
@@ -0,0 +1,31 @@
+﻿#this script written by Mathew Gallehawk to check memberships of users who have been inactive for 1 year
+#Variable decleration
+
+$InactiveDays = 365
+$Days = (Get-Date).Adddays( - ($InactiveDays))
+#ou path for end dated users
+$OUpath = 'OU=Redacted Users,OU=redacted,DC=aus,DC=redacted,DC=com,DC=au'
+$ou = 'OU=redacted,DC=aus,DC=redacted,DC=com,DC=au'
+redacted
+#export path for reports
+$ghost = '\\redacted\redacted\redacted\users\e103719\Documents\Reports\ghosts.csv'
+$GhostUsers = Get-ADUser -Filter { LastLogonTimeStamp -lt $Days -and enabled -eq $true } -SearchBase $ou -Properties LastLogonTimeStamp, Department, DisplayName | ? { ($_.distinguishedname -match '[A-Z]+\d{6}' ) } 
+$GhostUsers | foreach-object {
+   $u = Get-ADUser $_ ;
+   $Memership = Get-ADPrincipalGroupMembership $_ | select name;
+   $delimiter = "xxxxxxxxxxxxxxxx `n";
+   #$out= $u.UserPrincipalName +"`n"+ $Memership.name+"`n" +$delimiter;
+   $lastLog = $_ | select-object @{Name = "Date"; Expression = { [DateTime]::FromFileTime($_.lastLogonTimestamp).ToString('dd-MM-yyyy') } };
+   #$u.UserPrincipalName;
+   #$Memership
+   #$delimiter
+   #$out
+   #$lastLog
+   $u.UserPrincipalName | Out-file $ghost -Append;
+   $lastLog | Out-file $ghost -Append;
+   $Memership | Out-file $ghost -Append;
+   $delimiter | Out-file $ghost -Append;
+   #$out| Out-file $ghost -Append
+   # select-object Name, DisplayName,  Department ,@{Name="Date"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp).ToString('dd-MM-yyyy')}}, DistinguishedName| export-csv $ghost -notypeinformation
+}
+
